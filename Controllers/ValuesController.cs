b02f5fd620335a2d992e35353c3b5d3c7cb0dcc8
manith849 <101918134+manith849@users.noreply.github.com>
2022-09-29 11:13:39 +0530

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -48,6 +48,7 @@ namespace Dockersampleconfig.Controllers
             l.Add("Final Modification");
             l.Add("CTS CDX0102 Data Added");
             l.Add("New CDX0102 modified");
+            1.Add("New line added");
             return l;
         }
     }
